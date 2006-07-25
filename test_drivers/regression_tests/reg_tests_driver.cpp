c98c5f23569153666cc7b5cf21ea79a8352ea607
ntrifunovic <ntrifunovic@a809a056-fc17-0410-9590-b4f493f8b08e>
2006-07-25 18:11:42 +0000

    Added test case for bug [ 1528544 ] utf::next does not work correctly for 4-byte sequences
    git-svn-id: https://utfcpp.svn.sourceforge.net/svnroot/utfcpp@22 a809a056-fc17-0410-9590-b4f493f8b08e

--- test_drivers/regression_tests/reg_tests_driver.cpp
+++ test_drivers/regression_tests/reg_tests_driver.cpp
@@ -11,6 +11,7 @@ inline void check_impl (bool condition, const char* file, int line)
 #define check(c) check_impl(c, __FILE__, __LINE__);
 
 #include "r1_0Beta1/invalidutf8.h"
+#include "r1_0Beta1/basic_functionality.h"
 
 int main()
 {
@@ -18,4 +19,6 @@ int main()
   id_1524459();
   id_1525236();  
   id_1528369();
+//r1_0Beta1/basic_functionality.h
+  id_1528544();
 }
