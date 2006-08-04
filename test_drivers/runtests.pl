5eadfc935d4fa78821424029cfbb6700833a96f1
ntrifunovic <ntrifunovic@a809a056-fc17-0410-9590-b4f493f8b08e>
2006-08-04 00:38:14 +0000

    Added automated build scripts. Moved the tests Makefile to test_drivers directory
    git-svn-id: https://utfcpp.svn.sourceforge.net/svnroot/utfcpp@34 a809a056-fc17-0410-9590-b4f493f8b08e

---
+++ test_drivers/runtests.pl
@@ -0,0 +1,32 @@
+#! /usr/bin/perl
+
+$report_name = './report.txt';
+
+# Create the report file
+die if !open(REPORT, ">$report_name");
+
+# First, build everything
+print REPORT "==================Make output==================\n";
+close($report_name);
+
+`make >> $report_name`;
+die if !open(REPORT, ">>$report_name");
+print REPORT "==================End of Make output==================\n";
+print REPORT "\n";
+# Now, run individual tests and create the report
+print REPORT "==================Smoke Test ==================\n";
+close($report_name);
+chdir 'smoke_test';
+`./smoketest >> ../$report_name`;
+chdir '..';
+die if !open(REPORT, ">>$report_name");
+print REPORT "==================End of smoke test==================\n";
+print REPORT "\n";
+print REPORT "==================Regression Test ==================\n"; 
+close($report_name);
+chdir 'regression_tests';
+`./regressiontest >> ../$report_name`;
+chdir '..';
+die if !open(REPORT, ">>$report_name");
+print REPORT "==================End of regression test==================\n";
+print REPORT "\n";
